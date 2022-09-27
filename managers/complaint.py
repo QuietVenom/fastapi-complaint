d2d20f6e5d764cf0a0e32a70f9ba4d2077c2078b
Izcoatl Avila Marcos <43587292+QuietVenom@users.noreply.github.com>
2022-09-27 18:30:28 -0500

    Wise Trasactions

--- managers/complaint.py
+++ managers/complaint.py
@@ -4,12 +4,14 @@ import uuid
 from constants import TEMP_FILE_FOLDER
 from db import database
 from services.ses import SESService
-from models import RoleType, State, complaint
+from models import RoleType, State, complaint, transaction
 from services.s3 import S3Service
+from services.wise import WiseService
 from utils.helpers import decode_photo
 
 s3 = S3Service()
 ses = SESService()
+wise = WiseService()
 
 
 class ComplaintManager:
@@ -33,6 +35,7 @@ class ComplaintManager:
         complaint_data["photo_url"] = s3.upload(path, name, extension)
         os.remove(path)
         id_ = await database.execute(complaint.insert().values(complaint_data))
+        await ComplaintManager.issue_transaction(complaint_data["amount"], f"{user['first_name']} {user['last_name']}", user["iban"], id_)
         return await database.fetch_one(complaint.select().where(complaint.c.id == id_))
 
     @staticmethod
@@ -59,3 +62,17 @@ class ComplaintManager:
             .where(complaint.c.id == complaint_id)
             .values(status=State.rejected)
         )
+    
+    @staticmethod
+    async def issue_transaction(amount, full_name, iban, complaint_id):
+        quote_id = wise.create_quote(amount)
+        recipient_id = wise.create_recipient_account(full_name, iban)
+        transfer_id = wise.create_transfer(recipient_id, quote_id)
+        data = {
+            "quote_id": quote_id,
+            "transfer_id": transfer_id,
+            "target_account_id": str(recipient_id),
+            "amount": amount,
+            "complaint_id": complaint_id,
+        }
+        await database.execute(transaction.insert().values(**data))
